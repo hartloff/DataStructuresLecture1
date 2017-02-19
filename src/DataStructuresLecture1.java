0cfe3948810aa3365cab1e10f123e3edb2a65df1
jessehartloff <hartloff@buffalo.edu>
2017-02-19 02:09:55 -0500

    initial commit

---
+++ src/DataStructuresLecture1.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class DataStructuresLecture1 {
+
+
+    // 2 points
+    static boolean Q1(ArrayList<Double> inputList) {
+
+        // TODO: Return true if inputList is empty, false otherwise
+
+
+        return true;
+    }
+
+
+    // 3 points
+    static ArrayList<Integer> Q2(int value) {
+
+        // TODO: Return a new ArrayList containing value as its only element
+
+
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
